77e2523a1c276506f3845ed923bbfa72c660cbbe
gaivits <49124734+gaivits@users.noreply.github.com>
2021-10-15 17:06:34 +0700

    Add files via upload

---
+++ customer.php
@@ -0,0 +1,230 @@
+<?php
+	include "C:/xampp/htdocs/xampp/Azza/connects.php";
+	$conn= mysqli_connect("localhost","root","","customers") 
+	or die("Error: " . mysqli_error($conn));
+	mysqli_query($conn, "SET NAMES 'utf8' ");
+//query
+	$query=mysqli_query($conn,"SELECT COUNT(CUSTOMER_ID) FROM customer");
+	$row = mysqli_fetch_row($query);
+	$rows = $row[0];
+	$page_rows = 5;
+	$last = ceil($rows/$page_rows);
+	if($last < 1)
+	{
+		$last = 1;
+	}
+	$pagenum = 1;
+
+	if(isset($_GET['pn']))
+	{
+		$pagenum = preg_replace('#[^0-9]#', '', $_GET['pn']);
+	}
+	if ($pagenum < 1) 
+	{
+		$pagenum = 1;
+	}
+	else if ($pagenum > $last) 
+	{
+		$pagenum = $last;
+	}
+
+	$limit = 'LIMIT ' .($pagenum - 1) * $page_rows .',' .$page_rows;
+
+	$nquery=mysqli_query($conn,"SELECT * from  customer $limit");
+
+	$paginationCtrls = '';
+
+	if($last != 1)
+	{
+		if ($pagenum > 1) 
+		{
+			$previous = $pagenum - 1;
+			$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$previous.'" class="btn btn-info">Previous</a> &nbsp; &nbsp; ';
+
+		for($i = $pagenum-4; $i < $pagenum; $i++)
+		{
+			if($i > 0)
+			{
+				$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'" class="btn btn-primary">'.$i.'</a> &nbsp; ';
+			}
+		}
+	}
+
+	$paginationCtrls .= ''.$pagenum.' &nbsp; ';
+
+	for($i = $pagenum+1; $i <= $last; $i++)
+	{
+		$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'" class="btn btn-primary">'.$i.'</a> &nbsp; ';
+		if($i >= $pagenum+4)
+		{
+			break;
+		}
+	}
+
+if ($pagenum != $last) 
+{
+$next = $pagenum + 1;
+$paginationCtrls .= ' &nbsp; &nbsp; <a href="'.$_SERVER['PHP_SELF'].'?pn='.$next.'" class="btn btn-info">Next</a> ';
+}
+	}
+?>
+	
+	
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+<!-- Modal -->
+
+
+<div class="container">
+  <h2>AZZA-CUSTOMER-REGISTRATION</h2>
+  <!-- Trigger the modal with a button -->
+  <button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal">Add</button>
+  
+	  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-body">
+        <form id="CUSTOMER" name="CUSTOMER" method="POST">
+    	
+		<input type="text" autocomplete="off" id="datepicker" name="datepicker" class="form-control datepicker" placeholder="SELECT DATE">
+        <br>
+        <input type="text" autocomplete="off" id="timepicker" name="timepicker" width="276" placeholder="SELECT TIME">
+        <br>
+		<input type="number" autocomplete="off" min=0 step=0.01 id="AMOUNT" name="AMOUNT" placeholder="AMOUNT" >
+        <br>
+        <input type="text" autocomplete="off" id="JOB" name="JOB" placeholder="JOB" maxlength="255" >
+        <br>
+        <input type="text" autocomplete="off" id="EQUIPMENT" name="EQUIPMENT" placeholder="EQUIPMENT" maxlength="255">
+        <br>
+        <select id="USER" name="USER">
+    	<option value="">--SELECT USER--</option>
+        <?php
+        $records = mysqli_query($conn, "Select * from tbl_master_groupcode WHERE type='USER'");  // Use select query here 
+		while($data = mysqli_fetch_assoc($records))
+        {
+            echo "<option value='". $data['NAME'] ."'>" .$data['NAME'] ."</option>";  // displaying data in option menu
+		}
+		if($data['USER']=='มหาวิทยาลัย')
+		{
+				
+		}	
+    	?>  
+  		</select>
+        <br>
+        <input type="text" id="WE" name="WE" placeholder="WE" maxlength="255" value="Azza">
+        <br>
+        <select id="DEALER" name="DEALER">
+    	<option value="">--SELECT DEALER--</option>
+    	<?php
+        $records = mysqli_query($conn, "Select * from tbl_master_groupcode WHERE type='DEALER'");  // Use select query here 
+		while($data = mysqli_fetch_assoc($records))
+        {
+            echo "<option value='". $data['NAME'] ."'>" .$data['NAME'] ."</option>";  // displaying data in option menu
+		}	
+    	?>  
+  		</select>
+       
+        <select id="SUPPLIER" name="SUPPLIER" >
+    	<option value="">--SELECT SUPPLIER--</option>
+    	<?php
+        $records = mysqli_query($conn, "Select * from tbl_master_groupcode WHERE type='SUPPLIER'");  // Use select query here 
+		while($data = mysqli_fetch_assoc($records))
+        {
+            echo "<option value='". $data['NAME'] ."'>" .$data['NAME'] ."</option>";  // displaying data in option menu
+			
+		}	
+    	?>  
+  		</select>
+        
+    	</form>
+        
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-success" data-dismiss="modal" id="OK" name="OK" onclick="creates()">OK</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+     </div>
+  </div>
+</div>
+	
+	<div id="viewCustomer"></div>
+    
+     <div style="float:right;margin-right:50px;" id="pagination_controls"><?php echo $paginationCtrls; ?></div>
+    
+</body>
+</html>
+
+<script>
+$(function(){
+   $('.datepicker').datepicker({
+      format: 'yyyy/mm/dd'
+    });
+});
+ $('#timepicker').timepicker({
+            uiLibrary: 'bootstrap4'
+        });
+ 
+function creates(){
+	var DATE = $('#datepicker').val()
+	var TIME = $('#timepicker').val()
+	var AMOUNT = $('#AMOUNT').val()
+	var JOB = $('#JOB').val()
+	var EQUIPMENT = $('#EQUIPMENT').val()
+	var USER = $('#USER').val()
+	var DEALER = $('#DEALER').val()
+	var WE = $('#WE').val()
+	var SUPPLIER = $('#SUPPLIER').val()
+	if(DATE==='')
+	{
+		alert('Enter Date')
+		return false;
+	}
+	if(TIME==='')
+	{
+		alert('Enter TIME')
+		return false;
+	}
+	if(AMOUNT==='')
+	{
+		alert('Enter AMOUNT')
+		return false;
+	}
+	if(JOB==='')
+	{
+		alert('Enter JOB')
+		return false;
+	}
+	if(EQUIPMENT==='')
+	{
+		alert('Enter EQUIPMENT')
+		return false;
+	}
+	$.ajax({
+        type: "POST",
+        url: "customer_create.php",
+        data: {"DATE":DATE,"TIME":TIME,"AMOUNT":AMOUNT,"JOB":JOB,"EQUIPMENT":EQUIPMENT,"USER":USER,"DEALER":DEALER,"WE":WE,"SUPPLIER":SUPPLIER},
+        success: function(res) {
+            $('#viewCustomer').load('customer_show.php')
+        },
+		
+    });
+}
+$('#viewCustomer').load('customer_show.php')
+
+</script>
---
+++ customer_show.php
@@ -0,0 +1,115 @@
+<?php
+include "C:/xampp/htdocs/xampp/Azza/connects.php";
+
+$conn=new Databases;
+$conn = $conn->__construct();
+$query = "SELECT * FROM customer";
+$result = mysqli_query($conn, $query);
+
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" /> 
+<title>Customer</title>
+</head>
+
+
+<body>
+
+<table border='1' style="width:80%;margin-left:15%" class="table table-striped table-sm">
+  <thead align="center">
+    <tr>
+      <th>NO.</th>
+      <th>DATE</th>
+      <th>TIME</th>
+      <th>จัดซื้อประมาณ</th>
+      <th>โครงงาน</th>
+      <th>อุปกรณ์</th>
+      <th>USER</th>
+      <th>DEALER</th>
+      <th>WE</th>
+      <th>SUPPLIER</th>
+      <th colspan="2">ACTION</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+      <?php
+		
+		while($row = mysqli_fetch_array($result)) 
+		{$idx=0;
+		?>
+		<tr id=<?php echo $row["CUSTOMER_ID"] ;?>>
+    	<td width="2%" align="center"><nobr><?php echo $row[$idx]; ?></nobr></td>
+    	<td width="3%" align="center"><nobr><?php echo $row["CREATE_DATE"]; ?></nobr></td>
+    	<td width="3%" align="center"><nobr><?php echo $row["TIME"]; ?></nobr></td>
+    	<td width="5%" align="right"><nobr><?php echo number_format($row["AMOUNT"],2); ?></nobr></td>
+    	<td width="10%" align="left"><nobr><?php echo $row["JOB"]; ?></nobr></td>
+        <td width="10%" align="left"><nobr><?php echo $row["EQUIPMENT"]; ?></nobr></td>
+        <td width="6%" align="center"><nobr><?php echo $row["USER"]; ?></nobr></td>
+        <td width="10%" align="center"><nobr><?php echo $row["DEALER"]; ?></nobr></td>
+        <td width="5%" align="center"><nobr><?php echo $row["WE"]; ?></nobr></td>
+        <td width="5%" align="center"><nobr><?php echo $row["SUPPLIER"]; ?></nobr></td>
+        <td width="4%" align="center"><button class="btn btn-danger btn-sm" id="dels" name="dels" onclick="dels(<?php echo $row["CUSTOMER_ID"] ;?>)" >DEL</button></td>
+        <td width="4%" align="center" ><button class="btn btn-info btn-sm" id="edits" name="edits" data-toggle="modal" data-target="#myModal-2" onclick="edits(<?php echo $row["CUSTOMER_ID"] ;?>)" >EDIT</button></td>
+        
+		</tr>
+		<?php
+	$idx=$idx+1;
+	}
+?>
+    </tr>
+   </tbody>
+</table>
+
+ 
+</body>
+ 
+<div id="editCustomer"></div>
+  
+
+</body>
+</html>
+<script>
+
+function dels(ID)
+{
+	if(confirm('คุณต้องการลบหรือไม่?') )
+	{
+		$.ajax({
+			url : "customer_del.php",
+			type:"POST",
+			data : {"ID":ID},
+			success:function(res){
+					$("#delete"+ID).hide()
+					
+				}
+			})
+	}
+	window.location.href = "customer.php"
+}
+function edits(ID)
+{
+	var url = "customer_edit.php"
+	var SETDATA = {ID:ID}
+    $.post(url,SETDATA,function(res){
+			$('#editCustomer').html(res)
+			$('#editCustomer').modal('show')
+		})
+	
+}
+
+
+
+
+</script>
