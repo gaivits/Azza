23e94c34041a093c0f9847c9eff632d1f18946fd
gaivits <49124734+gaivits@users.noreply.github.com>
2021-10-20 15:20:14 +0700

    Delete customer_del.php

--- customer_del.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include "C:/xampp/htdocs/xampp/Azza/connects.php";
-	$conn=new Databases;
-	$conn = $conn->__construct();
-	$ID = $_POST["ID"];
-	$sql = "DELETE FROM customer WHERE CUSTOMER_ID='$ID'";	
-	mysqli_query($conn, $sql);
-	
-?>
