85f715a82ca6c5607692d6d18f9a611b4444533d
gaivits <49124734+gaivits@users.noreply.github.com>
2021-11-17 18:58:14 +0700

    Delete customer_add_user.php

--- customer_add_user.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include "C:/xampp/htdocs/xampp/Azza/connects.php";
-	$conn=new Databases;
-	$conn = $conn->__construct();
-	$ADD_USER = $_POST['ADD_USER'];
-	$sql = "INSERT INTO tbl_master_groupcode (ID,TYPE,NAME,REMARK) VALUES ('','USER','$ADD_USER','')";	
-	mysqli_query($conn,$sql);
-?>
