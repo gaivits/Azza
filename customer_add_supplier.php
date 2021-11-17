fa8434a861a2d76fa5170bd61d96ba1c6c4bcaf8
gaivits <49124734+gaivits@users.noreply.github.com>
2021-11-17 17:28:15 +0700

    Delete customer_add_supplier.php

--- customer_add_supplier.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include "C:/xampp/htdocs/xampp/Azza/connects.php";
-	$conn=new Databases;
-	$conn = $conn->__construct();
-	$ADD_SUPPLIER = $_POST['ADD_SUPPLIER'];
-	$sql = "INSERT INTO tbl_master_groupcode (TYPE,NAME,REMARK) VALUES ('SUPPLIER','$ADD_SUPPLIER','')";	
-	mysqli_query($conn,$sql);
-?>
