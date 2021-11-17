4bfec12215527a71f2fa1e3d371cbba1a864459c
gaivits <49124734+gaivits@users.noreply.github.com>
2021-11-17 17:26:53 +0700

    Delete customer_add_dealer.php

--- customer_add_dealer.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include "C:/xampp/htdocs/xampp/Azza/connects.php";
-	$conn=new Databases;
-	$conn = $conn->__construct();
-	$ADD_DEALER = $_POST['ADD_DEALER'];
-	$sql = "INSERT INTO tbl_master_groupcode (TYPE,NAME,REMARK) VALUES ('DEALER','$ADD_DEALER','')";	
-	mysqli_query($conn,$sql);
-?>
