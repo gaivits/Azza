ced97db51edd8f4477689e4450a4cb7ce5818154
gaivits <49124734+gaivits@users.noreply.github.com>
2021-12-03 17:16:15 +0700

    Add files via upload

--- customers.sql
+++ customers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2021 at 05:46 AM
+-- Generation Time: Dec 03, 2021 at 11:15 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.4.24
 
